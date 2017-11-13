2e29865438447853e237bf6101bd2e634ac6b85e
RBI4 <rebecca.inge@gmail.com>
2017-11-13 19:37:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
-function findMatching(drivers, name){
-  return drivers.filter(function(driverName) {
+function findMatching(list, name){
+  return list.filter(function(driverName) {
     return driverName.toLowerCase() == name.toLowerCase();
   });
 
